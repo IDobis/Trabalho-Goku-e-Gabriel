b03402869b94c90ce00614e056ab33d64af52e45
bryanperes33 <bryanperes52@gmail.com>
2026-04-08 00:16:19 -0300

    Create README.md. reverso.c

---
+++ Atividade 2/README.md. reverso.c
@@ -0,0 +1,3 @@
+O código coloca uma estrutura de pilha utilizando lista ordenada para inverter uma string.
+cada caractere da string é colocado na pilha usando a operação push, e posteriormente removida com pop, respeitando o principio Lifo.
+resultando na inversão completa da string de forma eficiente, com complexidade em linha.
